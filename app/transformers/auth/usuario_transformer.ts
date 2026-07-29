0f84d732b4ea18e6af0ec802101908cb6e89b1e0
alejandro1dsrl <muusicaa9812@gmail.com>
2026-07-29 14:28:02 -0500

    corregir contenido de UsuarioTransformer y mover productos_controller.ts a su carpeta correcta

--- app/transformers/auth/usuario_transformer.ts
+++ app/transformers/auth/usuario_transformer.ts
@@ -1,23 +1,22 @@
-import type Producto from '#models/inventario/producto'
+import type Usuario from '#models/auth/usuario'
 import { BaseTransformer } from '@adonisjs/core/transformers'
 
-export default class ProductoTransformer extends BaseTransformer<Producto> {
+export default class UsuarioTransformer extends BaseTransformer<Usuario> {
   toObject() {
     return this.pick(this.resource, [
       'id',
-      'nombre',
-      'descripcion',
-      'categoriaId',
-      'unidadMedida',
-      'esPerecedero',
-      'diasVencimientoEstimado',
-      'stockMinimo',
-      'ubicacionAutomaticaId',
-      'proveedorId',
-      'fotoUrl',
+      'nombreCompleto',
+      'correo',
+      'nombreUsuario',
+      'rol',
       'estado',
+      'extensionContacto',
+      'fotoPerfilUrl',
+      'debeCambiarContrasena',
+      'ultimoAccesoAt',
       'createdAt',
       'updatedAt',
+      'initials',
     ])
   }
 }
